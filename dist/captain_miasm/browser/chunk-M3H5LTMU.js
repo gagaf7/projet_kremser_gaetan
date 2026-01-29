bd8ea648551d1feda49d830e999d71c46ef5578a
gagaf7 <145220548+gagaf7@users.noreply.github.com>
2026-01-29 15:39:18 +0100

    Delete dist/captain_miasm directory

--- dist/captain_miasm/browser/chunk-M3H5LTMU.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./chunk-3YEJRNKW.js";import{Ga as r,La as n,q as t}from"./chunk-XVEGM77Z.js";var p=()=>{let o=t(n),i=t(r);return o.selectSnapshot(e.isAuthenticated)||o.selectSnapshot(e.token)?!0:i.navigate(["/sign-in"])};export{p as a};
